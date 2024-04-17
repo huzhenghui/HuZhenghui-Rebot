7422ad0ea3b73dc217698244144ce6eeb73a48c2
huzhenghui <hu@daonao.com>
2024-04-18 01:00:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ os.system(f'git clone https://code.openxlab.org.cn/HuZhenghui/HuZhenghui-Robot.g
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, device_map='auto', mmap_persisted_file=True)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
